5d3b0b337646184ae92f3119dd7de21425a5f35e
Michael Großklaus <mail@mgrossklaus.de>
2021-01-08 01:26:12 +0100

    added rate limiter

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const compression = require("compression");
 const dotProp = require("dot-prop");
+const RateLimit = require("express-rate-limit");
+
+const limiter = new RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 30,
+  skip: (req) => {
+    return path.extname(req.url).length > 0;
+  },
+});
+const loginLimiter = new RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
 
 const Router = require("./src/router");
 const Locales = require("./src/locales");
@@ -23,7 +36,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(csrf({ cookie: true }));
 app.use(compression());
+app.set("trust proxy", true);
+app.use("/", limiter);
+app.use("/login", loginLimiter);
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "twig");
