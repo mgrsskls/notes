93aad865ee302101d15cf087e8f09d58ee8ea022
Michael Großklaus <mail@mgrossklaus.de>
2021-03-13 13:37:25 +0100

    changed db connection

--- src/db.js
+++ src/db.js
@@ -1,13 +1,19 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-client.connect();
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("connected");
+  }
+});
 
 module.exports = {
   getNotes: (query, cb) => {
